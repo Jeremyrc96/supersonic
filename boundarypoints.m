0a5788ca666ff6ff87d06681a2a172c503dab213
BWesely21 <bohdanwesely@me.com>
2017-04-27 12:52:53 -0700

    Delete boundarypoints.m

--- boundarypoints.m
+++
@@ -1,8 +0,0 @@
-function [xp,yp] = boundarypoints(x,m2,theta,n)  
-    xp = ones(n,1); yp = xp;
-    for i = 1:n
-    xp(i) = (-m2(i,n)*x(i,n)) / (tand(theta(i,n)) - m2(i,n));
-    yp(i) = xp(i)* tand(theta(i,n));
-    end
-    plot(xp,yp)
-end
