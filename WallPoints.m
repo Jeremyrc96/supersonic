bdcc5162020426aab65c56464ba251d384131133
BWesely21 <bohdanwesely@me.com>
2017-04-27 12:25:01 -0700

    Add files via upload

--- WallPoints.m
+++ WallPoints.m
@@ -1,31 +1,28 @@
-function [Xp, Yp] = WallPoints(X,Y,m2,T,N)
+function [Xp, Yp] = WallPoints(X,Y,m2,T,N,Xi,Yi)
+% computes the wall points and plots characteristics from last inner points to wall 
 
-Xi = 0;
-Yi = 1;
-Yp = zeros(1,N);
-Xp = zeros(1,N);
-A = [1 -tand(T(1,N+1)); 1 -m2(1,N)];
+Yp = zeros(1,N); % initialize wall points
+Xp = zeros(1,N); 
+A = [1 -tand(T(1,N+1)); 1 -m2(1,N)]; % matrixes to solve 2x2 system for first wall point
 B = [Yi-tand(T(1,N+1))*Xi Y(1,N)-m2(1,N)*X(1,N)]';
 A = linsolve(A,B);
-Xp(1) = A(1); Yp(1) = A(2); 
+Xp(1) = A(2); Yp(1) = A(1); 
 
-% C = {'c','b','r','g','y',[.5 .6 .7],[.8 .2 .6]};
+fplot(@(x) Y(1,N) + m2(1,N)*(x-X(1,N)),[X(1,N) Xp(1)],'b') % plots first characteristics
+hold on
+fplot(@(x) -(Y(1,N) + m2(1,N)*(x-X(1,N))),[X(1,N) Xp(1)],'b')
+hold on
 
-% fplot(@(x) Yi + tand(T(1,N+1))*(x-Xi),[0,5],C{1})
-% hold on
-% fplot(@(x) Y(1,N) + m2(1,N)*(x-X(1,N)),[0,5],C{1})
-% hold on
-
-for i = 2:N
+for i = 2:N % all other points
 A = [1 -tand(T(i,N+1)); 1 -m2(i,N)];
 B = [Yp(i-1)-tand(T(i,N+1))*Xp(i-1) Y(i,N)-m2(i,N)*X(i,N)]';
 A = linsolve(A,B);
 Xp(i) = A(2); Yp(i) = A(1);  
 
-% fplot(@(x) Yp(i-1) + tand(T(i,N+1))*(x-Xp(i-1)),[0,5],C{i})
-% hold on
-% fplot(@(x) Y(i,N) + m2(i,N)*(x-X(i,N)),[0,5],C{i})
-% hold on
+fplot(@(x) Y(i,N) + m2(i,N)*(x-X(i,N)),[X(i,N) Xp(i)],'b')
+hold on
+fplot(@(x) -(Y(i,N) + m2(i,N)*(x-X(i,N))),[X(i,N) Xp(i)],'b')
+hold on
 
 end
 end
