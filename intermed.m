c13e6945e00852b3a13da18eb5495e4297095a40
BWesely21 <bohdanwesely@me.com>
2017-04-27 12:52:25 -0700

    Delete intermed.m

--- intermed.m
+++
@@ -1,8 +0,0 @@
-function [minus,plus] = intermed(alp,mu,n)
-a = n+1; minus = ones(a,a); plus = minus;
-for j = 1:a %Rows
-for i = 1:a %Columns
-    plus(j,i)= alp(j,i) + mu(j,i);
-    minus(j,i)= alp(j,i) - mu(j,i);
-end
-end
